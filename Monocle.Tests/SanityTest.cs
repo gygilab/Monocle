c789949feab7f52990940d34176487ff9a78e9d0
rrad <rrad@localhost>
2019-08-26 15:47:32 -0700

    add test project

---
+++ Monocle.Tests/SanityTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Monocle.Tests
+{
+    public class SanityTest
+    {
+        [Fact]
+        public void TestTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
