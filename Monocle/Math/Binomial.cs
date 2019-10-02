d4a2c8b69580f6db9dd983b80fb29581d0f8105c
dschweppe <devin.k.schweppe@gmail.com>
2019-10-02 15:47:40 -0400

    Update EstimateCarbons to uniprot 2019_08, add comments

--- Monocle/Math/Binomial.cs
+++ Monocle/Math/Binomial.cs
@@ -5,11 +5,24 @@ namespace Monocle.Math
 {
     public static class Binomial
     {
+        /// <summary>
+        /// Binomial probability mass function
+        /// </summary>
+        /// <param name="n">trials</param>
+        /// <param name="k">successes</param>
+        /// <param name="p">probability (e.g. 1.1% for carbon C13)</param>
+        /// <returns></returns>
         public static double P(int n, int k, double p)
         {
             return (NCr(n, k) * System.Math.Pow(p, k) * System.Math.Pow((1.0 - p), (n - k)));
         }
 
+        /// <summary>
+        /// Calculate binomial coefficient
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="r"></param>
+        /// <returns></returns>
         private static double NCr(int n, int r)
         {
             double retVal = 1;
