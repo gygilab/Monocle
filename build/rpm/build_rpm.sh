678e80fdbf413d417758cd764cb41ae3c4ef8b56
rrad <rrad@localhost>
2020-03-22 16:48:50 -0700

    set permissions for docker environment

--- build/rpm/build_rpm.sh
+++ build/rpm/build_rpm.sh
@@ -1,7 +1,13 @@
+set -e
+set -x
 
 rm -rf ~/rpmbuild
 mkdir -p ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SOURCES/monocle-ms,SPECS,SRPMS}
 cp -R * ~/rpmbuild/SOURCES/monocle-ms/
+
+# rpm requires known user. e.g. docker environment might use a different user.
+chown -R `whoami`.`whoami` ~/rpmbuild
+
 pushd ~/rpmbuild/SOURCES/
 pwd
 tar czvf monocle-ms.rpm.tar.gz monocle-ms
