27258e62984e062666e0c068963019b2765f3bce
rrad <rrad@localhost>
2020-03-22 18:55:20 -0700

    fix tarball name for build deb

--- build/deb/build_deb.sh
+++ build/deb/build_deb.sh
@@ -1,11 +1,13 @@
 
 # put files in a folder with the same name
 # as the package name for debuild
+rm -rf /tmp/monocle-ms/
 mkdir -p /tmp/monocle-ms/
 
 # The current directlry may already be tmp so ignore warning
 cp -R * /tmp/monocle-ms/ || true
-cd /tmp/monocle-ms
+
+pushd /tmp/monocle-ms
 
 # Automatically set Version in changelog
 sed -i -r "s/monocle-ms \(\S+\) /monocle-ms ($VERSION-$BUILD_NUMBER) /" build/deb/debian/changelog
@@ -16,11 +18,13 @@ cp build/deb/Makefile .
 cd Monocle.CLI
 dotnet publish -c Release -r linux-x64 -o Monocle
 cd ../../
-tar czvf monocle-ms_$VERSION-$BUILD_NUMBER.orig.tar.gz monocle-ms
+tar czvf monocle-ms_$VERSION.orig.tar.gz monocle-ms
 cd monocle-ms
 cp -R build/deb/debian . 
 debuild -us -uc
 
+popd
+
 # Copy files to artifacts dir
 mkdir -p deb
-cp ../*.deb deb/
+mv /tmp/monocle-ms*.deb deb/
